411fe03b618da4f1bee2d293d251774b86d19e6f
Jonathan Bloxsom <Jonathan.Bloxsom@softwire.com>
2017-12-04 16:08:59 +0000

    created an addition calculator

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
-console.log('Calculator On')
+const readline = require('readline-sync');
+console.log('Calculator On');
+console.log('Enter an integer:');
+const number1 = parseInt(readline.prompt());
+console.log('Enter another integer:');
+const number2 = parseInt(readline.prompt());
+console.log(number1+number2);
+// comment
+
+/*
+also a comment
+ */
