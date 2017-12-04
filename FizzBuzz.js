5fd33c77a6ba2d83f1e02d5baa44b8a48fd6491d
Jonathan Bloxsom <Jonathan.Bloxsom@softwire.com>
2017-12-04 16:56:49 +0000

    completed fizzfezzbuzzbangbong

--- FizzBuzz.js
+++ FizzBuzz.js
@@ -1,10 +1,20 @@
-for (let number = 1; number < 101; number++) {
+for (let number = 1; number < 106; number++) {
     let output = '';
     if (number % 3 === 0) {
         output += 'Fizz';
     }
+    if (number % 13 === 0) {
+        output += 'Fezz'
+    }
     if (number % 5 === 0) {
         output += 'Buzz';
+    }
+    if (number % 7 === 0) {
+        output += 'Bang';
+    }
+
+    if (number % 11 === 0) {
+        output = 'Bong'
     }
     if (output === '') {
         console.log(number);
