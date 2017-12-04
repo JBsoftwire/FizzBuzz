399bce4c6cc1d1d8c8ba9cb9bac00b09336f76a8
Jonathan Bloxsom <Jonathan.Bloxsom@softwire.com>
2017-12-04 16:14:18 +0000

    replaced adder with multiplier

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ console.log('Enter an integer:');
 const number1 = parseInt(readline.prompt());
 console.log('Enter another integer:');
 const number2 = parseInt(readline.prompt());
-console.log(number1+number2);
+console.log(number1*number2);
 // comment
 
 /*
